4072f591a11e369c3b418944e655bdbda79373a7
Tommy Vercetti <7903172+tommyvct@users.noreply.github.com>
2023-03-21 15:15:57 -0500

    fix DLL

--- some_lib/some_lib.hpp
+++ some_lib/some_lib.hpp
@@ -2,11 +2,23 @@
 
 #include <string>
 
+// dllexport boilerplate
+#if defined(SOME_LIB_DLL)
+#   if defined(_MSC_VER)
+#       if defined(SOME_LIB_COMPILE)
+#           define SOME_LIB_API __declspec(dllexport)
+#       else
+#           define SOME_LIB_API __declspec(dllimport)
+#       endif
+#   elif defined(__GNUC__) || defined(__clang__)
+#       if defined(SOME_LIB_COMPILE)
+#           define SOME_LIB_API __attribute__ ((visibility ("default")))
+#       endif
+#   endif
+#endif
 
-#ifdef SOME_LIB_EXPORT
-    #define SOME_LIB_API __declspec(dllexport)
-#else
-    #define SOME_LIB_API 
+#if !defined(SOME_LIB_API)
+#   define SOME_LIB_API 
 #endif
 
 SOME_LIB_API std::string repeat(int times, std::string what);
